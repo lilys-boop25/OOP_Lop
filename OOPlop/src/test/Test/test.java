35ccb981b3998f4e2f99671626b07df5cb7093f7
lilys-boop25 <ly.nk235600@sis.hust.edu.vn>
2024-12-01 23:04:47 +0700

    add file

---
+++ OOPlop/src/test/Test/test.java
@@ -0,0 +1,25 @@
+package test.Test;
+
+import main.CongTy.CongTy;
+import main.NhanVien.*;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CongTy congTy = new CongTy("Công ty ABC", 500000);
+
+        NhanVien cb1 = new CanBoCoHuu("Nguyễn Văn A", 5000, 2.5);
+        NhanVien cb2 = new CanBoHopDong("Trần Thị B", 7000);
+        NhanVien gd = new GiamDoc("Lê Văn C", 10000, 3.0, 2000);
+        NhanVien tp = new TruongPhong("Hoàng Minh D", 8000, 2.0, 1500);
+
+        congTy.themNV(cb1);
+        congTy.themNV(cb2);
+        congTy.themNV(gd);
+        congTy.themNV(tp);
+
+        congTy.inThongTin();
+        System.out.println("Tổng lương: " + congTy.tinhTongLuong());
+	}
+
+}
